cb971f83e21d6a110edb5342b77d9d23ce6108e0
shreyansh0727 <106952584+shreyansh0727@users.noreply.github.com>
2024-06-23 16:30:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template
 import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
 app = Flask(__name__)
 
@@ -9,30 +9,23 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return True  # Allow all file types
-
-# Route for the home page
 @app.route('/')
 def home():
     files = os.listdir(app.config['UPLOAD_FOLDER'])
     return render_template('index.html', files=files)
 
-# Route to handle file uploads
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'file' not in request.files:
+    if 'files[]' not in request.files:
         return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
+    files = request.files.getlist('files[]')
+    for file in files:
+        if file.filename == '':
+            continue
         filename = file.filename
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('home'))
-    return redirect(request.url)
+    return redirect(url_for('home'))
 
-# Route to download a file
 @app.route('/uploads/<filename>')
 def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
